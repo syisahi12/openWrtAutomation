8c1034af3bfe51685f2683fcbeafa32df16a5dae
syisahi12 <syisahi4696@gmail.com>
2021-03-31 06:51:32 +0700

    untuk restart lan jika router disconnect

---
+++ networkrest.sh
@@ -0,0 +1,16 @@
+while true
+do
+	if ping -c 1 192.168.1.2 > /dev/null
+	then
+		logger -p daemon.info -t "network[EXECUTED]" "Connected!"
+		echo "[OK] Router Connected!"
+		break
+	else
+		logger -p daemon.info -t "network[EXECUTED]" "Reconnecting!"
+		echo "[!] Router Disconnect"
+		echo "[!] Connecting..."
+		/etc/init.d/network restart
+		ifup lan
+		sleep 10
+	fi
+done
